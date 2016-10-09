1a73e40f47dfead8fea2b11a177acafe81fce696
mmohl <mmohammadl.lpkia@gmail.com>
2016-10-09 12:20:14 +0700

    initial

---
+++ app/configs/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+  // start session
+  session_start();
+
+  // include slim
+  require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . 'vendor'  . DIRECTORY_SEPARATOR . 'autoload.php';
+
+  // make new app
+  $app = new \Slim\App([
+    'settings' => [
+      'displayErrorDetails' => true
+    ]
+  ]);
+
+  // get the container for hold customs data
+  $container = $app->getContainer();
+
+  // holds Controllers
+  $container['AuthController'] = function($container) {
+    return new \App\Controllers\AuthController($container);
+  };
+
+  // include routing setting
+  require __DIR__ . '/Routes.php';
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+  // includ Main setting
+  require __DIR__ . DIRECTORY_SEPARATOR .'app' . DIRECTORY_SEPARATOR . 'configs'. DIRECTORY_SEPARATOR . 'Main.php';
+
+  // running slim
+  $app->run();
